fa404df470d3f66a038b89ee9f617b38b820eac1
onizuka8 <gheri666@hotmail.it>
2024-11-17 22:52:11 +0000

    skip intro

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,45 @@ function App() {
     <>
       <div>
         {showTypeAnimation && (
-          <TypeAnimation
-            sequence={[
-              // Same substring at the start will only be typed once, initially
-              "Search For \nMajestic Mountains",
-              1300,
-              "Search For \nVibrant Forests",
-              1300,
-              "Search For \nExpansive Deserts",
-              1300,
-              "Search For \nThe Best Images",
-              1300,
-              () => {
+          <>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed once, initially
+                "Search For \nMajestic Mountains",
+                1300,
+                "Search For \nVibrant Forests",
+                1300,
+                "Search For \nExpansive Deserts",
+                1300,
+                "Search For \nThe Best Images",
+                1300,
+                () => {
+                  setShowImageSearch(true);
+                  setTypeAnimation(false);
+                },
+              ]}
+              speed={50}
+              cursor={false}
+              style={{
+                fontSize: "2em",
+                fontWeight: "bold",
+                whiteSpace: "pre-line",
+                height: "195px",
+                display: "block",
+              }}
+            />
+
+            <button
+              style={{ background: "rgba(250, 190, 0, .8)" }}
+              onClick={() => {
                 setShowImageSearch(true);
                 setTypeAnimation(false);
-              },
-            ]}
-            speed={50}
-            cursor={false}
-            style={{
-              fontSize: "2em",
-              fontWeight: "bold",
-              whiteSpace: "pre-line",
-              height: "195px",
-              display: "block",
-            }}
-          />
+              }}
+              className="search-now"
+            >
+              Search Now
+            </button>
+          </>
         )}
 
         {showImageSearch && <ImageSearch />}
