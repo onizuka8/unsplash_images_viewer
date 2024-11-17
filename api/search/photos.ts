5f3a47ce74bc0e48962b01470e3333a1bda4b33d
onizuka8 <gheri666@hotmail.it>
2024-11-17 21:45:25 +0000

    add tsconfig.json for api (vercel function)

--- api/search/photos.ts
+++ api/search/photos.ts
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { query, page = 1, perPage = 20 } = req.query;
 
   if (!query) {
